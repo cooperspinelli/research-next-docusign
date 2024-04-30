a371f55888f33746b42e69dbb5be8c0469e80098
Anissa Nishioka <ani.nishioka@gmail.com>
2024-04-29 20:16:45 -0700

    get access token

--- app/api/docusign/apiKey/route.ts
+++ app/api/docusign/apiKey/route.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-import { ApiClient } from 'docusign-esign';
+import { NextRequest, NextResponse } from 'next/server';
 import process from 'process';
 import fs from "fs";
 import path from 'path';
@@ -7,21 +6,29 @@ import path from 'path';
 
 // Endpoints for /api/apiKey
 
-export async function GET() {
-  let dsApiClient = new ApiClient();
-  dsApiClient.setBasePath(process.env.BASE_PATH!);
+export async function POST(request: NextRequest) {
+  const reqBody = await request.json();
+  const encodedKeys = btoa(`${process.env.INTEGRATION_KEY}:${process.env.SECRET_KEY}`);
+  const resp = await fetch('https://account-d.docusign.com/oauth/token', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${encodedKeys}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=authorization_code&code=${reqBody.code}`
+  });
+  return NextResponse.json(await resp.json());
+  // const results = await dsApiClient.requestJWTUserToken(
+  //   process.env.INTEGRATION_KEY!,
+  //   process.env.USER_ID!,
+  //   ["signature"],
+  //   fs.readFileSync(path.join(__dirname, "private.key")),
+  //   3600
+  // );
+  // console.log(results.body);
+  // const key = results.body.access_token;
+  // const expires_at = Date.now() + (results.body.expires_in - 60) * 1000;
 
-  const results = await dsApiClient.requestJWTUserToken(
-    process.env.INTEGRATION_KEY!,
-    process.env.USER_ID!,
-    ["signature"],
-    fs.readFileSync(path.join(__dirname, "private.key")),
-    3600
-  );
-  console.log(results.body);
-  const key = results.body.access_token;
-  const expires_at = Date.now() + (results.body.expires_in - 60) * 1000;
-
-  return NextResponse.json({ key, expires_at });
+  // return NextResponse.json({ key, expires_at });
 
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+
+import { FormEvent } from "react";
 import Form from "./components/Form";
 
 export default function Home() {
+  async function handleSubmit(evt: FormEvent) {
+    evt.preventDefault();
+    const codeInput = document.querySelector('#code') as HTMLInputElement;
+    const resp = await fetch('http://localhost:3001/api/docusign/apiKey', {
+      method: 'POST',
+      body: JSON.stringify({code: codeInput.value})
+    });
+    console.log(await resp.json());
+  }
+
   return (
-    <Form/>
+    <div>
+      <Form />
+      <form>
+        <label htmlFor="">Code</label>
+        <input type="text" name="code" id="code"/>
+        <button onClick={handleSubmit}>Button</button>
+      </form>
+    </div>
   );
 }
